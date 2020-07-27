6a463c42027f6cfc733d961e39b7a8f5b271335c
Jalissa Williams <jalissa_williams@yahoo.com>
2020-07-27 17:44:16 -0400

    Updates

---
+++ precache-manifest.dedcb61675b6f31e8d6399eb5f6f207f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247b63c6428573d4df5e17d4ecd478bc",
+    "url": "/Congress/index.html"
+  },
+  {
+    "revision": "49bea8b687fd252f6b88",
+    "url": "/Congress/static/css/main.36daf65a.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/Congress/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Congress/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49bea8b687fd252f6b88",
+    "url": "/Congress/static/js/main.17de79f3.chunk.js"
+  },
+  {
+    "revision": "624fbde38793578018d8",
+    "url": "/Congress/static/js/runtime-main.7f061685.js"
+  },
+  {
+    "revision": "083bb1572025de8c962dd19eb242a09d",
+    "url": "/Congress/static/media/capitol.083bb157.JPG"
+  },
+  {
+    "revision": "b74461d4c2bb4ea6ac74668233ccafb3",
+    "url": "/Congress/static/media/caps.b74461d4.svg"
+  }
+]);
